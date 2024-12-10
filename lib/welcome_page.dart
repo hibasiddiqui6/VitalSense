e4a93923c0dc5f3afa177e4870a75bf16b3a10b4
RedrexOx <raifali434@gmail.com>
2024-12-10 12:03:23 +0500

    3 pages done login patient, welcome and registration of patient.

--- lib/welcome_page.dart
+++ lib/welcome_page.dart
@@ -2,94 +2,153 @@ import 'package:flutter/material.dart';
 import 'login_patient.dart'; // Import the login page for user
 import 'login_specialist.dart'; // Import the login page for user
 
-
 class WelcomePage extends StatelessWidget {
   const WelcomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          // Background image
-          Positioned.fill(
-            child: Image.asset(
-              'assets/background.png', // Replace with your image path
-              fit: BoxFit.cover,
-            ),
+      body: Center(
+        child: Container(
+          width: 412, // Fixed width for the app
+          height: 917, // Fixed height for the app
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.black), // Optional: border for visibility
+            color: Colors.white, // Background color
+            borderRadius: BorderRadius.circular(20),
           ),
-          // Content
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 60),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const SizedBox(), // Space for alignment
-
-                // Welcome Text
-                Column(
-                  children: [
-                    Text(
-                      'Welcome to VitalSense',
-                      style: TextStyle(
-                        color: Color(0xFF373737),
-                        fontSize: 32,
-                        fontFamily: 'Inter',
-                        fontWeight: FontWeight.w700,
-                      ),
-                    ),
-                    const SizedBox(height: 6),
-                    Text(
-                      'Your personal health companion for a healthier you!',
-                      style: TextStyle(
-                        color: Color(0xFF343434),
-                        fontSize: 19,
-                        fontFamily: 'Inter',
-                        fontWeight: FontWeight.w400,
-                      ),
-                    ),
-                  ],
+          child: Stack(
+            children: [
+              // Background image
+              Positioned.fill(
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.asset(
+                    'assets/background.png', // Replace with your image path
+                  fit: BoxFit.cover,
+                  )
+                  
                 ),
-                
-                const SizedBox(height: 40),
+              ),
+              // Content
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 38, vertical: 95),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  
+                  children: [
+                    const SizedBox(), // Space for alignment
+                   
+                    // Welcome Text
+                    Column(
+                      children: [
+                        Text(
+                          'Welcome to VitalSense',
+                          style: TextStyle(
+                            color: const Color(0xFF373737),
+                            fontSize: 32,
+                            fontFamily: 'Inter',
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                        const SizedBox(height: 6),
+                        Text(
+                          'Your personal health companion for a healthier you!',
+                          style: TextStyle(
+                            color: const Color(0xFF343434),
+                            fontSize: 19,
+                            fontFamily: 'Inter',
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ],
+                    ),
 
-                // Role Selection
-                Container(
-                  constraints: const BoxConstraints(maxWidth: 336),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      // Patient Role Box
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const PatientLogin(),
+                    const SizedBox(),
+                    
+                    // Role Selection
+                    
+                    Container(
+                      constraints: const BoxConstraints(maxWidth: 336),
+                      margin: const EdgeInsets.symmetric(vertical: 120),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          // Patient Role Box
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const PatientLogin(),
+                                ),
+                              );
+                            },
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                          
+                              children: [
+                                Text(
+                                  'Continue as',
+                                  style: TextStyle(
+                                    color: const Color(0xFF343434),
+                                    fontSize: 28,
+                                    fontFamily: 'Inter',
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                                ),
+                                
+                                const SizedBox(height: 22),
+                                Container(
+                                
+                                  padding: const EdgeInsets.symmetric(
+                                    horizontal: 39,
+                                    vertical: 63,
+                                  ),
+                                  decoration: BoxDecoration(
+                                    
+                                    color: Color.fromRGBO(247, 253, 245, 1).withOpacity(0.6),
+                                    borderRadius: BorderRadius.circular(20),
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color: Colors.black.withOpacity(0.30),
+                                        offset: const Offset(20, 10),
+                                        blurRadius: 20,
+                                      ),
+                                    ],
+                                  ),
+                                  child: Text(
+                                    'Patient',
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 20,
+                                      fontFamily: 'Inter',
+                                      fontWeight: FontWeight.w400,
+                                    ),
+                                  ),
+                                ),
+                              ],
                             ),
-                          );
-                        },
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              'Continue as',
-                              style: TextStyle(
-                                color: Color(0xFF343434),
-                                fontSize: 28,
-                                fontFamily: 'Inter',
-                                fontWeight: FontWeight.w400,
-                              ),
-                            ),
-                            const SizedBox(height: 21),
-                            Container(
+                          ),
+                          // Health Specialist Role Box
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const SpecialistLogin(),
+                                ),
+                              );
+                            },
+                            child: Container(
+                            
+                              margin: const EdgeInsets.only(top: 55),
                               padding: const EdgeInsets.symmetric(
-                                horizontal: 42,
-                                vertical: 63,
+                                horizontal: 28,
+                                vertical: 47.5,
                               ),
                               decoration: BoxDecoration(
-                                color: Colors.white,
+                                color: Color.fromRGBO(247, 253, 245, 1).withOpacity(0.6),
                                 borderRadius: BorderRadius.circular(20),
                                 boxShadow: [
                                   BoxShadow(
@@ -100,7 +159,8 @@ class WelcomePage extends StatelessWidget {
                                 ],
                               ),
                               child: Text(
-                                'Patient',
+                                'Health\nSpecialist',
+                                textAlign: TextAlign.center,
                                 style: TextStyle(
                                   color: Colors.black,
                                   fontSize: 20,
@@ -109,57 +169,18 @@ class WelcomePage extends StatelessWidget {
                                 ),
                               ),
                             ),
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
-                      // Health Specialist Role Box
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const SpecialistLogin(),
-                            ),
-                          );
-                        },
-                        child: Container(
-                          margin: const EdgeInsets.only(top: 55),
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: 30,
-                            vertical: 51,
-                          ),
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(20),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.black.withOpacity(0.25),
-                                offset: const Offset(20, 10),
-                                blurRadius: 20,
-                              ),
-                            ],
-                          ),
-                          child: Text(
-                            'Health\nSpecialist',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 20,
-                              fontFamily: 'Inter',
-                              fontWeight: FontWeight.w400,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
+                    ),
+                  
+                    const SizedBox(), // Space for alignment
+                  ],
                 ),
-
-                const SizedBox(), // Space for alignment
-              ],
-            ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
